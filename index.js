f967a54e3a4481f4d733b45a3c6b31e8c4079a72
Glenn Nguyo <glennnguyo1000@gmail.com>
2022-06-05 21:59:51 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+function moveDodgerLeft(){
+ const dodger = document.getElementById('dodger')
+ dodger.style.left = `${parseInt(dodger.style.left)-1}px`;
+}
+function moveDodgerRight(){
+    const dodger = document.getElementById('dodger')
+    dodger.style.left = `${parseInt(dodger.style.left)+1}px`;
+   }
